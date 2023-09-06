c2199af60457de03f4a61ad47a933ad95031720c
patahak542 <144149356+patahak542@users.noreply.github.com>
2023-09-06 14:34:09 +0700

    Add files via upload

--- answer.sh
+++ answer.sh
@@ -1,17 +1,19 @@
 default_menu() {
 clear
 
-echo "Ратанак Test"
-echo "============"
-echo "Join or Exit "
-echo "1] Join"
-echo "0] exit"
+echo -e "\033[1;32m             Ратанак Test\033[0m"
+echo -e "\033[1;32m************************************\033[0m"
+echo -e "\033[1;32mJoin or Exit\033[0m"
+echo -e "\033[1;32m1• Join\033[0m"
+echo -e "\033[1;31m0• Exit\033[0m"
+
+echo -e "\033[1;32m************************************\033[0m"
 }
 
 while true; do
 default_menu
 echo " "
-echo ">>> Select option ==> "
+echo -e "\033[1;32m>>> Select option ==>\033[0m"
 read choice
 
 case $choice in
@@ -20,12 +22,16 @@ echo
 echo "Exited"
 echo
 exit 0
+bold="\033[1m"
+green_bg="\033[42m"
+red_bg="\033[41m"
+reset="\033[0m"
 ;;
 1)
-echo "Good choice now you have completed the test I would love to give you this https://t.me/+-Cu4O1Uhk6llMDRl to join our group for discussion."
+echo -e "\033[1;32mGood choice now you have been invited into our society, I would love to give you this \033[33;5m    https://t.me/+-Cu4O1Uhk6llMDRl\033[0m  \033[1;32mto join our group for discussion.\033[0m"
 ;;
 *)
-echo "invalid choice please select valid choice."
+echo -e "\033[1;32mInvalid Choice please enter the choice \033[0m"
 ;;
 esac
 
